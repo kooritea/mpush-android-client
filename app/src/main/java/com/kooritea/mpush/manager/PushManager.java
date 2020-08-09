c1c7368e1113573b45fb2ee4f63a8cdc4c5f182b
kooritea <ochatokori@gmail.com>
2020-08-10 07:41:41 +0800

    不在让消息自定义channel，而是使用priority定义优先级

--- app/src/main/java/com/kooritea/mpush/manager/PushManager.java
+++ app/src/main/java/com/kooritea/mpush/manager/PushManager.java
@@ -33,6 +33,9 @@ public class PushManager {
         this.context = context;
         Log.d("debug",context.toString());
         notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        this.createNotificationChannel("low","低优先级",NotificationManager.IMPORTANCE_LOW,"priority=low");
+        this.createNotificationChannel("default","普通优先级",NotificationManager.IMPORTANCE_DEFAULT,"priority=default");
+        this.createNotificationChannel("high","高优先级",NotificationManager.IMPORTANCE_HIGH,"priority=high");
     }
 
     private void createNotificationChannel(String channelId, String channelName, int importance, String description) {
@@ -53,18 +56,26 @@ public class PushManager {
     }
 
     public void push(Message message){
-        String channelId = "default";
-        String channelName = "default";
-        String description = "default";
-        int importance = NotificationManager.IMPORTANCE_HIGH;
+        String channelId;
+        int importance;
         try{
-            if(message.getData().getExtra().getString("channelId").length() > 0){
-                channelId = message.getData().getExtra().getString("channelId");
-                channelName = channelId;
-                description = "消息中定义的频道";
+            switch (message.getData().getExtra().getString("priority")){
+                case "default":
+                    channelId = "default";
+                    importance = NotificationCompat.PRIORITY_DEFAULT;
+                    break;
+                case "low":
+                    channelId = "low";
+                    importance = NotificationCompat.PRIORITY_LOW;
+                    break;
+                default:
+                    channelId = "high";
+                    importance = NotificationCompat.PRIORITY_HIGH;
             }
-        }catch (JSONException ex){}
-        this.createNotificationChannel(channelId,channelName,importance,description);
+        }catch (JSONException ex){
+            channelId = "high";
+            importance = NotificationCompat.PRIORITY_HIGH;
+        }
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context, channelId)
             .setSmallIcon(R.drawable.ic_launcher);
         if(message.getData().getText().length() > 0){
@@ -74,24 +85,20 @@ public class PushManager {
             builder.setContentText(message.getData().getDesp());
         }
 
-        Intent intent = new Intent(this.context, DetailActivity.class);;
+        Intent intent;
         try{
-            if(message.getData().getExtra().getString("scheme").length() > 0){
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(message.getData().getExtra().getString("scheme")));
-            }
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(message.getData().getExtra().getString("scheme")));
         }catch (JSONException ex){
-
+            intent = new Intent(this.context, DetailActivity.class);
+            intent.putExtra("message",message.toString());
         }
-        intent.putExtra("message",message.toString());
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this.context);
         stackBuilder.addParentStack(MainActivity.class);
         stackBuilder.addNextIntent(intent);
         PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(pendingIntent);
 
-
-
-        builder.setPriority(NotificationCompat.PRIORITY_HIGH)
+        builder.setPriority(importance)
             .setAutoCancel(true);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this.context);
         notificationManager.notify((int) System.currentTimeMillis(), builder.build());
